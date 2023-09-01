2d855d606a3ed255cb68dc2e033f9931db75012c
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2023-09-01 15:43:14 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <title>Red News Reader</title>
+                <title>News Links</title>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180.png" />
