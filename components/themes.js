a15d07b25f77a483af5d345d2465bc1d41c89187
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2023-09-01 15:38:03 +0530

    Update themes.js

--- components/themes.js
+++ components/themes.js
@@ -1,10 +1,10 @@
 const themes = [
     {title: "Россия",   link: "nation",        color: "#ff4f40"},
-     {title: "Бизнес",   link: "business",      color: "#91A6FF"}
+     {title: "Бизнес",   link: "business",      color: "#91A6FF"},
     {title: "World",    link: "world",         color: "#35b789"},
     {title: "Наука",    link: "scitech",       color: "#FFB86F"},
      {title: "Культура", link: "entertainment", color: "#DCED31"},
-    {title: "Спорт",    link: "sports",        color: "#88CCF1"},
+    {title: "Спорт",    link: "sports",        color: "#88CCF1"}
 
 ]
 
